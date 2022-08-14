c6357d621f068be0738e1b317dbfdaa963d55750
Jared McMahan <jaredm788@gmail.com>
2022-08-14 13:57:32 -0600

    commit tree 450c0a8eb34d8d305e45c8b1f97423b0030a298d

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+const scuberLocation = 42
+const pickupInFt = 264
+
+function distanceFromHqInBlocks(pickupBlock) {
+  if (pickupBlock > 42) {
+    return pickupBlock - scuberLocation
+  } else if (pickupBlock < 42) {
+    return scuberLocation - pickupBlock
+  }
+
+}
+
+function distanceFromHqInFeet(number) {
+   return distanceFromHqInBlocks(number) * pickupInFt
+   
+
+}
+
+function distanceTravelledInFeet(scuberLocation,destination1) {
+    if (destination1 > scuberLocation) {
+        return (destination1 - scuberLocation) * pickupInFt 
+    }   else {
+        return (scuberLocation - destination1) * pickupInFt
+    }
+
+  }
+
+  function  calculatesFarePrice(scuberLocation, destination1) {
+    let calculatesFarePrice
+    let feetTraveled = distanceTravelledInFeet(scuberLocation,destination1) 
+    if (feetTraveled < 400) {
+        return calculatesFarePrice = 0
+    } else if (feetTraveled > 400 && feetTraveled < 2000) {
+        calculatesFarePrice = feetTraveled - 400 
+        return (calculatesFarePrice) * 0.02
+    } else if (feetTraveled > 2000 && feetTraveled < 2499) {
+        calculatesFarePrice = 25
+        return calculatesFarePrice
+    } else if (feetTraveled > 2500) {
+        return 'cannot travel that far'
+    }
+  }
+  
